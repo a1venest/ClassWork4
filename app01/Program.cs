fe22569f5ad3a396580e7b4c8b457329dd5b1ffe
Aivenest <aivenest@gmail.com>
2022-09-29 22:25:37 +0500

    app01 add

---
+++ app01/Program.cs
@@ -0,0 +1,16 @@
+﻿int GetMulti(int a, int b)
+{
+    return a*b;
+}
+
+// ************************************
+// Основная программа
+
+// int number1 = new Random().Next(0,100);
+// int number2 = new Random().Next(0,100);
+
+int number1 = 5;
+int number2 = 2;
+
+
+Console.WriteLine(GetMulti(number1,number2));
